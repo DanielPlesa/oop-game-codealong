00e458435535489b1f9a575a6b1077f86a7efa01
Daniel Plesa <danielplesa@Daniels-MacBook-Pro.local>
2022-09-15 17:13:08 +0200

    create and move obstacles

--- js/main.js
+++ js/main.js
@@ -1,64 +1,115 @@
-class Game {
 
+class Game {
     constructor(){
         this.player = null; //will store an instance of the class Player
+        this.obstacles = []; //will store instances of the class Obstacle
     }
-    start (){
-        
+
+    start(){
         this.player = new Player();
         this.attachEventListeners();
+
+        //moving obstacles
+        setInterval(() => {
+            this.obstacles.forEach( (obstacleInstance) => {
+                obstacleInstance.moveDown();
+            });
+        }, 50);
+
+        setInterval(() => {
+            //creates new obstacle
+            const newObstacle = new Obstacle();
+            this.obstacles.push(newObstacle);
+        }, 3000);
     }
     attachEventListeners(){
-        document.addEventListener("keydown", (event)=>{
-            if (event.key === "ArrowLeft"){
+        document.addEventListener("keydown", (event) => {
+            if(event.key === "ArrowLeft"){
                 this.player.moveLeft();
-            }else if (event.key === "ArrowRight"){
+            }else if(event.key === "ArrowRight"){
                 this.player.moveRight();
             }
         });
-
     }
 }
 
+
 class Player {
-    constructor (){
-        this.positionX = 45;
+    constructor(){
+        this.positionX = 50;
         this.positionY = 0;
+        this.width = 20;
+        this.height = 10;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement('div');
+
+        // set id and css
+        this.domElement.id = "player";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+
+        // append to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
+    }
+    moveLeft(){
+        this.positionX-=5;
+        this.domElement.style.left = this.positionX + "vw";
+    }
+    moveRight(){
+        this.positionX += 5;
+        this.domElement.style.left = this.positionX + "vw";
+
+       let futurePositionX = this.positionX + "x";
+
+    if ( futurePositionX < rightScreenBound ) {
+        this.PositionX = futurePostionX;
+    } else {
+        this.PositionX = rightScreenBound;
+    }
+    }
+}
+
+
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 90;
         this.width = 10;
         this.height = 10;
         this.domElement = null;
 
         this.createDomElement();
-
     }
     createDomElement(){
-        //create dom element;
-       this.domElement = document.createElement('div');
+        // create dom element
+        this.domElement = document.createElement('div');
 
-        //set id and css;
-        this.domElement.id = "player";
+        // set id and css
+        this.domElement.className = "obstacle";
         this.domElement.style.width = this.width + "vw";
-        this.domElement.style.height = this.height + "vh"
+        this.domElement.style.height = this.height + "vh";
         this.domElement.style.bottom = this.positionY + "vh";
         this.domElement.style.left = this.positionX + "vw";
 
-        //append the elem to the dom
+        // append to the dom
         const boardElm = document.getElementById("board");
         boardElm.appendChild(this.domElement)
-
     }
-    moveLeft(){
-       
-this.positionX = this.positionX -1;
-this.domElement.style.left = this.positionX + "vw";
+    moveDown(){
+        this.positionY--;
+        this.domElement.style.bottom = this.positionY + "vh";
     }
+}
 
-    moveRight(){
-        
-        this.positionX = this.positionX +1;
-        this.domElement.style.left = this.positionX + "vw";
-    }
-} 
+const game = new Game();
+game.start();
 
-const game =new Game();
-game.start ();
+
