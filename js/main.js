bb4c79ddec4d244dd6c71e84db8a0fecaaec0b1a
Daniel Plesa <danielplesa@Daniels-MacBook-Pro.local>
2022-09-15 15:12:55 +0200

    implement display and move player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,64 @@
-console.log("my js file was loaded..")
+class Game {
+
+    constructor(){
+        this.player = null; //will store an instance of the class Player
+    }
+    start (){
+        
+        this.player = new Player();
+        this.attachEventListeners();
+    }
+    attachEventListeners(){
+        document.addEventListener("keydown", (event)=>{
+            if (event.key === "ArrowLeft"){
+                this.player.moveLeft();
+            }else if (event.key === "ArrowRight"){
+                this.player.moveRight();
+            }
+        });
+
+    }
+}
+
+class Player {
+    constructor (){
+        this.positionX = 45;
+        this.positionY = 0;
+        this.width = 10;
+        this.height = 10;
+        this.domElement = null;
+
+        this.createDomElement();
+
+    }
+    createDomElement(){
+        //create dom element;
+       this.domElement = document.createElement('div');
+
+        //set id and css;
+        this.domElement.id = "player";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh"
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+
+        //append the elem to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
+
+    }
+    moveLeft(){
+       
+this.positionX = this.positionX -1;
+this.domElement.style.left = this.positionX + "vw";
+    }
+
+    moveRight(){
+        
+        this.positionX = this.positionX +1;
+        this.domElement.style.left = this.positionX + "vw";
+    }
+} 
+
+const game =new Game();
+game.start ();
